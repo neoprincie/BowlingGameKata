cd5461439b1f3b0622b033cd28f15d73fe8b253e
neoprincie <bprince7@vt.edu>
2017-06-12 21:10:13 -0400

    Initla commit

---
+++ BowlingGameKata/BowlingGameKata.Tests/BowlingTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameKata.Tests
+{
+    [TestClass]
+    public class BowlingTests
+    {
+        private BowlingScorer scorer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            scorer = new BowlingScorer();
+        }
+
+        [TestMethod]
+        public void AllZeroes_ShouldScoreZero()
+        {
+            RollMany(0, 20);
+
+            Assert.AreEqual(0, scorer.GetScore());
+        }
+
+        [TestMethod]
+        public void AllOnes_ShouldScoreTwenty()
+        {
+            RollMany(1, 20);
+
+            Assert.AreEqual(20, scorer.GetScore());
+        }
+
+        [TestMethod]
+        public void AllTwos_ShouldScoreForty()
+        {
+            RollMany(2, 20);
+
+            Assert.AreEqual(40, scorer.GetScore());
+        }
+
+        [TestMethod]
+        public void Spare()
+        {
+            scorer.Roll(4);
+            scorer.Roll(6);
+            scorer.Roll(3);
+
+            Assert.AreEqual(16, scorer.GetScore());
+        }
+
+        private void RollMany(int pins, int repititions)
+        {
+            for (var i = 0; i < repititions; i++)
+                scorer.Roll(pins);
+        }
+    }
+}
---
+++ BowlingGameKata/BowlingGameKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BowlingGameKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BowlingGameKata.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8db65759-016e-464e-bc13-a3e8945e1c1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
