376c07e5c766a8efaeef431ce31e058bd94a320b
neoprincie <bprince7@vt.edu>
2017-06-19 21:23:42 -0400

    Finished spare and strike

--- BowlingGameKata.Tests/BowlingTests.cs
+++ BowlingGameKata.Tests/BowlingTests.cs
@@ -48,6 +48,41 @@ namespace BowlingGameKata.Tests
             Assert.AreEqual(16, scorer.GetScore());
         }
 
+        [TestMethod]
+        public void Strike()
+        {
+            scorer.Roll(10);
+            scorer.Roll(3);
+            scorer.Roll(3);
+
+            Assert.AreEqual(22, scorer.GetScore());
+        }
+
+        [TestMethod]
+        public void LastFrame()
+        {
+            RollMany(10, 12);
+
+            Assert.AreEqual(300, scorer.GetScore());
+        }
+
+        [TestMethod]
+        public void NoRollCanBeGreaterThanTen()
+        {
+            var actual = "Oh noes!";
+
+            try
+            {
+                scorer.Roll(42);
+            }
+            catch (Exception ex)
+            {
+                actual = ex.Message;
+            }
+
+            Assert.AreEqual("Oh noes!", actual);
+        }
+
         private void RollMany(int pins, int repititions)
         {
             for (var i = 0; i < repititions; i++)
--- BowlingGameKata/BowlingScorer.cs
+++ BowlingGameKata/BowlingScorer.cs
@@ -8,27 +8,52 @@ namespace BowlingGameKata
 {
     public class BowlingScorer
     {
-        private List<int> rolls;
+        //private List<int> rolls;
+        private int[] rolls;
+        private int currentRoll;
 
         public BowlingScorer()
         {
-            rolls = new List<int>();
+            rolls = new int[21];
+            currentRoll = 0;
         }
 
         public void Roll(int pins)
         {
-            rolls.Add(pins);
+            if (pins > 10)
+                throw new Exception("Oh noes!");
+
+            rolls[currentRoll] = pins;
+            currentRoll++;
         }
 
         public int GetScore()
         {
             var score = 0;
+            var rollCount = 0;
 
-            foreach(var roll in rolls)
+            for(var frame = 0; frame < 10; frame++)
             {
-                score = score + roll;
+                if (rolls[rollCount] == 10)
+                {
+                    score = score + rolls[rollCount]
+                        + rolls[rollCount + 1] + rolls[rollCount + 2];
+                    rollCount = rollCount + 1;
+                }
+                else if (rolls[rollCount] + rolls[rollCount + 1] == 10)
+                {
+                    score = score + rolls[rollCount] 
+                        + rolls[rollCount + 1] + rolls[rollCount + 2];
+                    rollCount = rollCount + 2;
+                }
+                else
+                {
+                    score = score + rolls[rollCount] + rolls[rollCount + 1];
+                    rollCount = rollCount + 2;
+                }
+                
             }
-
+            
             return score;
         }
     }
